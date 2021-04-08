9c711734b20ba22b4674795ac141bbac2eddf83e
Madcrafty <53935032+Madcrafty@users.noreply.github.com>
2021-04-09 00:02:51 +1000

    imported package from Shadergraph, game is prison break dekcuf

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Raycast : MonoBehaviour
+{
+    public TextMeshProUGUI output;
+    //public GameObject lookfocus;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 500, LayerMask.GetMask("Default")) == true)
+        {
+            output.text = hitInfo.transform.name;
+            //lookfocus.transform.position = hitInfo.transform.position;
+            if (hitInfo.transform.GetComponent<Button>() != null)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    hitInfo.transform.GetComponent<Button>().onClick.Invoke();
+                }
+                
+            }
+        }
+    }
+}
